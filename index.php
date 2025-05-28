93f48d1d579d26388bb7f4c17adf78d32205fdb6
agugus1 <atcejas@escuelasproa.edu.ar>
2025-05-28 12:30:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_datos = "mi_basedatos";
+
+$conn = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+?>
+
+
 </body>
 </html>
